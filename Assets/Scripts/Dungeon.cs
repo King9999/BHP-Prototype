35c290cae7ae0bb2004965acfb6a4a8609eb434a
Mike Murray <mikemurray056@gmail.com>
2024-07-31 10:56:44 -0400

    Working on dungeon creation

---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* A dungeon consists of procedurally generated rooms. The creation of dungeons occurs in this script. */
+public class Dungeon : MonoBehaviour
+{
+    public List<Room> roomPrefabs;         //master list of room prefabs.
+    public List<Room> dungeonRooms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateDungeon()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Rooms are the building blocks for creating dungeons. Each room can have connect points, which are used to attach rooms together.
+ * A room can have multiple points, but which ones are enabled is random. */
+public class Room : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
