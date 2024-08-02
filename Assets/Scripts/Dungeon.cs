bd544656655a84c396413e6c7f1e8a63e128b389
Mike Murray <mikemurray056@gmail.com>
2024-08-02 14:42:59 -0400

    Rooms now attach themselves to a connect point
    This isn't the behaviour I want, but I'm trying to figure out how to add rooms in a way that makes sense to me.

--- Assets/Scripts/Dungeon.cs
+++ Assets/Scripts/Dungeon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 /* A dungeon consists of procedurally generated rooms. The creation of dungeons occurs in this script. 
@@ -24,10 +25,74 @@ public class Dungeon : MonoBehaviour
 
     public void CreateDungeon()
     {
-        Room room = Instantiate(roomPrefabs[0]);
+        int roomCount = 2;
 
-        room.connectPoints[0].gameObject.SetActive(true);
+        while (dungeonRooms.Count < roomCount)
+        {
+            Room room = Instantiate(roomPrefabs[0]);
 
-        dungeonRooms.Add(room);
+            //room.connectPoints[0].gameObject.SetActive(true);
+            //if this room has no active connect points, activate a random one
+            /*if (dungeonRooms.Count <= 1 && room.DeadEnd())
+            {
+                int randPoint = Random.Range(0, room.connectPoints.Count);
+                room.ActivateConnectPoint(randPoint);
+                room.connectPoints[randPoint].isSelected = true;
+            }
+            else
+            {*/
+            //we don't want dead ends early on.
+            if (dungeonRooms.Count < 1 /*&& room.DeadEnd()*/)
+            {
+                int randPoint = Random.Range(0, room.connectPoints.Count);
+                room.ActivateConnectPoint(randPoint);
+                room.connectPoints[randPoint].isSelected = true;
+            }
+            else
+            {
+                //check the last room that was added for a connect point, then add the new room there.
+                bool pointFound = false;
+                int i = 0;
+                int lastRoom = dungeonRooms.Count - 1;
+                while (!pointFound && i < dungeonRooms[lastRoom].connectPoints.Count)
+                {
+                    if (dungeonRooms[lastRoom].connectPoints[i].isSelected)
+                    {
+                        //connect room to this point.
+                        pointFound = true;
+                        room.gameObject.transform.position = dungeonRooms[lastRoom].connectPoints[i].point.position;
+                        //room.connectPoints[i].isSelected = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+
+                //select the next connect point
+                pointFound = false;
+                i = 0;
+
+                while (!pointFound && i < room.connectPoints.Count)
+                {
+                    //int randPoint = Random.Range(0, room.connectPoints.Count);
+                    //room.connectPoints[randPoint].isSelected = true;
+                    if (room.connectPoints[i].point.gameObject.activeSelf)
+                    {
+                        pointFound = true;
+                        room.connectPoints[i].isSelected = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            //}
+
+            dungeonRooms.Add(room);
+            //i++;
+        }
     }
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -6,7 +6,14 @@ using UnityEngine;
  * A room can have multiple points, but which ones are enabled is random. */
 public class Room : MonoBehaviour
 {
-    public List<Transform> connectPoints;
+    [System.Serializable]
+    public class ConnectPoint
+    {
+        public Transform point;
+        public bool isConnected;        //if true, a room is attached to this point.
+        public bool isSelected;         //if true, a room is going to be connected to this point.
+    }
+    public List<ConnectPoint> connectPoints;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +30,7 @@ public class Room : MonoBehaviour
     private void ActivateConnectPoints()
     {
         List<bool> activePoints = new List<bool>();
-        foreach (Transform t in connectPoints)
+        foreach (ConnectPoint t in connectPoints)
         {
             if (Random.value <= 0.35f)
                 activePoints.Add(true);
@@ -35,9 +42,33 @@ public class Room : MonoBehaviour
         {
             if (activePoints[i] == true)
             {
-                connectPoints[i].gameObject.SetActive(true);
+                connectPoints[i].point.gameObject.SetActive(true);
             }
         }
         
+    }
+
+    public void ActivateConnectPoint(int point)
+    {
+        connectPoints[point].point.gameObject.SetActive(true);
+    }
+
+    //checks for rooms with no connect points. The first half number of rooms generated must have at least one connect point.
+    public bool DeadEnd()
+    {
+        int inactivePointsCount = 0;
+        for(int i = 0; i < connectPoints.Count; i++)
+        {
+            if (!connectPoints[i].point.gameObject.activeSelf)
+            {
+                inactivePointsCount++;
+            }
+           
+        }
+
+        if (inactivePointsCount >= connectPoints.Count)
+            return true;
+        else
+            return false;
     }
 }
