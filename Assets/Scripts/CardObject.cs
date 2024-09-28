9224a6719ba9fa0b1974bdd497f2ff16ea83a612
Mike Murray <mikemurray056@gmail.com>
2024-09-28 12:22:10 -0400

    Card now animates on hover, and returns to original position when mouse leaves card

--- Assets/Scripts/CardObject.cs
+++ Assets/Scripts/CardObject.cs
@@ -10,6 +10,34 @@ public class CardObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 {
     public Card cardData;       //handles card activation also.
     public Sprite cardSprite;
+    bool revertCardOn, animateCardOn, coroutineActive, origPosCaptured;
+    Vector3 originalPos;
+
+    /*void OnEnable()
+    {
+        originalPos = this.transform.position;
+        Debug.Log("Original card Pos: " + originalPos);
+    }*/
+
+    void Update()
+    {
+        //need to do this in update instead of Start because the card position isn't correct until after the first frame update apparently.
+        if (!origPosCaptured)
+        {
+            origPosCaptured = true;
+            originalPos = transform.position;
+        }
+
+        if (revertCardOn && !coroutineActive)
+        {
+            StartCoroutine(RevertCard());
+        }
+
+        if (animateCardOn && !coroutineActive)
+        {
+            StartCoroutine(AnimateCard());
+        }
+    }
 
     public void ShowCard(bool toggle)
     {
@@ -18,22 +46,32 @@ public class CardObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerExit(PointerEventData pointer)
     {
-        if (cardData != null)
-        {
+        revertCardOn = true;
+        animateCardOn = false;
+        //if (cardData != null)
+        //{
             HunterManager hm = Singleton.instance.HunterManager;
             hm.ChangeHunterMenuState(hm.hunterMenuState = HunterManager.HunterMenuState.SelectCard);
-        }
+        //}
+        //if (!coroutineOn)
+            //StartCoroutine(RevertCard());
     }
 
     //displays detailed info about card and plays a short animation
     public void OnPointerEnter(PointerEventData pointer)
     {
-        if (cardData != null)
-        {
-            HunterManager hm = Singleton.instance.HunterManager;
+        animateCardOn = true;
+        revertCardOn = false;
+        //if (cardData != null)
+        //{
+        HunterManager hm = Singleton.instance.HunterManager;
             hm.ChangeHunterMenuState(hm.hunterMenuState = HunterManager.HunterMenuState.CardDetails);
             GetDetails(cardData);
-        }
+        //}
+
+        //animate the card. Card shifts up a little
+        //if (!coroutineOn)
+            //StartCoroutine(AnimateCard());
     }
 
     void GetDetails(Card cardData)
@@ -47,6 +85,40 @@ public class CardObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
             hm.ui.cardDetailsText.text = cardData.cardDetails_combat;
         else
             hm.ui.cardDetailsText.text = cardData.cardDetails_field;
+    }
+
+    IEnumerator AnimateCard()
+    {
+        //StopCoroutine(RevertCard());
+        coroutineActive = true;
+        //originalPos = transform.position;
+        Vector3 newPos = new Vector3(originalPos.x, originalPos.y + 30, originalPos.z);
+        float moveSpeed = 200;
+
+        while (transform.position.y < newPos.y)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z);
+            yield return null;
+        }
+        coroutineActive = false;
+        animateCardOn = false;
+    }
+
+    IEnumerator RevertCard()
+    {
+        //StopCoroutine(AnimateCard());
+        coroutineActive = true;
+        float moveSpeed = 200;
+        //Vector3 originalPos = transform.position;
+        //Vector3 newPos = new Vector3(transform.position.x, transform.position.y - 30, transform.position.z);
+
+        while (transform.position.y > originalPos.y)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime, transform.position.z);
+            yield return null;
+        }
+        coroutineActive = false;
+        revertCardOn = false;
     }
 
 }
